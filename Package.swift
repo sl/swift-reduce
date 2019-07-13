e590ff6b7d47c84999f9cbe0a6b6879a67a9be3a
Sam Lazarus <tyu.sam@gmail.com>
2019-07-12 20:36:35 -0400

    Add ios13 to platforms list.

--- Package.swift
+++ Package.swift
@@ -6,7 +6,8 @@ import PackageDescription
 let package = Package(
   name: "SwiftReduce",
   platforms: [
-    .macOS(.v10_15)
+    .macOS(.v10_15),
+    .iOS(.v13),
   ],
   products: [
     // Products define the executables and libraries produced by a package, and make them visible to other packages.
---
+++ examples/SwiftReduceExample/SwiftReduceExample/Movies.swift
@@ -0,0 +1,9 @@
+//
+//  Movies.swift
+//  SwiftReduceExample
+//
+//  Created by Sam Lazarus on 7/12/19.
+//  Copyright © 2019 Sam Lazarus. All rights reserved.
+//
+
+import Foundation
